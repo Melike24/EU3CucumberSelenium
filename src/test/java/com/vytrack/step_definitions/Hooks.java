e4cdce6d80e5225f7df6fcc0321e1b722da4515d
Melike24 <nurullah24yunus@gmail.com>
2020-10-20 22:21:21 +0200

    added hooks

---
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -0,0 +1,15 @@
+package com.vytrack.step_definitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+    @Before
+    public void setUp(){
+        System.out.println("\tThis is coming from BEFORE");
+    }
+    @After
+    public void tearDown(){
+        System.out.println("\tThis is coming from AFTER");
+    }
+}
